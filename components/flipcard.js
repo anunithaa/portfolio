d2a3feb323232a40a00fd16a116ffe1d6e74869a
anunithaa <anunithaa.rajakumaresan@gmail.com>
2025-07-23 14:08:52 -0400

    mobile ui updates

--- components/flipcard.js
+++ components/flipcard.js
@@ -10,27 +10,29 @@ export default function FlipCard({
   listThree,
 }) {
   return (
-    <div className={styles.card}>
-      <div className={styles.inner}>
-        <div className={styles.front}>
-          <div className={styles.content}>
-            <div className={styles.icon}>{icon}</div>
-            <span className={`${styles.frontTag} ${styles[type]}`}>
-              {frontText}
-            </span>
+    <div class="cardsWrapper">
+      <div className={styles.card}>
+        <div className={styles.inner}>
+          <div className={styles.front}>
+            <div className={styles.content}>
+              <div className={styles.icon}>{icon}</div>
+              <span className={`${styles.frontTag} ${styles[type]}`}>
+                {frontText}
+              </span>
+            </div>
           </div>
-        </div>
 
-        <div className={styles.back}>
-          <div className={styles.experienceTop}>
-            <h2>{years}</h2>
-            <p>of experience</p>
+          <div className={styles.back}>
+            <div className={styles.experienceTop}>
+              <h2>{years}</h2>
+              <p>of experience</p>
+            </div>
+            <ul className={styles.detailsList}>
+              <li>{listOne}</li>
+              <li>{listTwo}</li>
+              <li>{listThree}</li>
+            </ul>
           </div>
-          <ul className={styles.detailsList}>
-            <li>{listOne}</li>
-            <li>{listTwo}</li>
-            <li>{listThree}</li>
-          </ul>
         </div>
       </div>
     </div>
